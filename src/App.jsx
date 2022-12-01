04245a9c9bb52f3c5e90b92c6c7daa98005b6a8b
Nizam Uddin Ahmed <45719824+NizamSadaf@users.noreply.github.com>
2022-12-01 22:10:10 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Header from './Components/Header';
 import Home from './Components/Home';
 import Coins from './Components/MyCoins';
 import Exchange from './Components/Exchange';
-import CoinDetails from './Components/CoinDetails';
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/coins' element={<Coins/>}/>
-          <Route path='/coin/:id' element={<CoinDetails/>}/>
           <Route path='/exchange' element={<Exchange/>}/>
         </Routes>
       </Router>
